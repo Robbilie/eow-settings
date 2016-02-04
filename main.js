0279ecccf6d17c6b97da49c711fdf74330d8c93c
Robert Schuh <github@eneticum.de>
2016-02-04 01:37:27 +0100

    added self update

--- main.js
+++ main.js
@@ -30,6 +30,7 @@
 
 		tabs.selectTab("Settings");
 
+		updateCore();
 		loadRepositoryList(updateList);
 
 		(Widget.loadData("windows") || []).map(url => openWidget(url));
@@ -75,6 +76,82 @@
 						repo.loadAs("text", tree["index.json"].hash, (err, fdata) => {
 							var list = JSON.parse(fdata);
 							cb(list);
+						});
+					});
+				});
+			});
+		}
+
+		function updateCore () {
+			var modes = require('js-git/lib/modes');
+			var fs = require("fs");
+			var localpackage = JSON.parse(fs.readFileSync("package.json"));
+			var repo = {};
+			var githubName = "Robbilie/eow-core";
+			var githubToken = Widget.loadData("accesstoken");
+			
+			require('js-github/mixins/github-db')(repo, githubName, githubToken);
+			require('js-git/mixins/create-tree')(repo);
+			require('js-git/mixins/mem-cache')(repo);
+			require('js-git/mixins/read-combiner')(repo);
+			require('js-git/mixins/formats')(repo);
+
+			repo.readRef("refs/heads/master", (err, headHash) => {
+				repo.loadAs("commit", headHash, (err, commit) => {
+					repo.loadAs("tree", commit.tree, (err, tree) => {
+						repo.loadAs("text", tree["package.json"].hash, (err, fdata) => {
+							var coredata = JSON.parse(fdata);
+							console.log(coredata);
+							console.log(tree);
+
+							var cnt = 0;
+
+							var storeTree = (path, tree) => {
+								cnt += Object.keys(tree).length;
+								// create path if non existant
+								try {
+									console.log("writing path: " + path);
+									fs.mkdirSync(path);
+								} catch (e) {}
+
+								for(var f in tree) {
+									if(modes.isFile(tree[f].mode)) {
+										saveFile(repo, tree[f].hash, path + "/" + f, modes.isBlob(tree[f].mode) ? "blob" : "text");
+									} else if(modes.toType(tree[f].mode) == "tree") {
+										loadTree(repo, tree[f].hash, path + "/" + f);
+									}
+								}
+							};
+
+							var saveFile = (repo, hash, name, type) => {
+								repo.loadAs(type, hash, (err, fdata) => {
+									console.log("writing " + type + ": " + name);
+									fs.writeFileSync(name, fdata);
+									cnt--;
+									isDone();
+								});
+							};
+
+							var loadTree = (repo, hash, path) => {
+								repo.loadAs("tree", hash, (err, tree) => {
+									storeTree(path, tree);
+									cnt--;
+									isDone();
+								});
+							};
+
+							var done = false;
+							var isDone = () => {
+								if(cnt === 0) {
+									console.log("finally done");
+									alert("Successfully updated EOW");
+								}
+							};
+
+
+							if(localpackage.version != coredata.version) {
+								storeTree(".", tree);
+							}
 						});
 					});
 				});
